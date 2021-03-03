b984618c21f342e1cbc2a464e6ae194aabd65efc
PrinceAPK <princeakoto26@gmail.com>
2021-03-03 20:56:25 +0200

    Initial commit

---
+++ src/com/princeapk/Main.java
@@ -0,0 +1,18 @@
+package com.princeapk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // row
+        int i;
+        // column
+        int j;
+        for (i=1; i<5; ++i){
+            for (j=1; j<=i; ++j){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+    }
+
+}
+}
